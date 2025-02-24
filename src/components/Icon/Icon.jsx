2729c046b6e6a1f71cb51c9a11c1d3315a30100f
klimisnotavailable <klimgrigor28@gmail.com>
2025-02-24 14:00:55 +0200

    icons

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,7 +1,7 @@
 const Icon = ({ id, style, width, height }) => {
   return (
     <svg className={style} width={width} height={height}>
-      <use href={`../../../public/icons/icons.svg#${id}`} />
+      <use href={`../../img/icons/icons#svg#${id}`} />
     </svg>
   );
 };
